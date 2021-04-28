69facb4b727356e556d3c80567a2839ca6ea6499
Sophie Engle <sjengle@cs.usfca.edu>
2021-04-28 14:19:46 -0700

    Setting up placeholder tests.

---
+++ src/test/java/HeaderServerTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The tests for the HeaderServer homework assignment.
+ */
+public class HeaderServerTest {
+
+	/**
+	 * Placeholder class since there are no unit tests for this assignment. 
+	 */
+	@Nested
+	public class A_NoTests {
+		/**
+		 * Always fails because this homework cannot be unit tested.
+		 */
+		@Test
+		public void testFail() {
+			Assertions.fail("This extra credit homework may not be unit tested. It must be demonstrated in lab!");
+		}
+	}
+}
